ecc34a67d214cce10036e28799a80dd3e7060108
algitbana <albanadhimitri@hotmail.com>
2025-05-21 10:19:18 +0200

    Set Mongoose strictQuery to true

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const mongoose = require('mongoose');
+const mongoose = require('mongoose');mongoose.set('strictQuery', true); 
 const jwt = require('jsonwebtoken');
 const cors = require('cors');
 const app = express();
