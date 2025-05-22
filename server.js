21d51fca391b9e42113808b90f6f388a6fdaea8b
algitbana <albanadhimitri@hotmail.com>
2025-05-22 16:30:04 +0200

    Update server.js

--- server.js
+++ server.js
@@ -102,4 +102,4 @@ app.get('/api/messages/client', verifyToken, async (req, res) => {
   res.json(messages);
 });
 
-app.listen(process.env.PORT || 3000, () => console.log('Server running on port ' + (process.env.PORT || 3000))); 
+app.listen(process.env.PORT, () => console.log('Server running on port ' + process.env.PORT)); 
